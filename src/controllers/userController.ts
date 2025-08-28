ecdf4e55527250f3ce8ada93f5187602521bd0ca
AmiChanDev <gamageamantha@gmail.com>
2025-08-28 08:59:31 +0530

    Add user retrieval, update, and deletion functionality in userController and userRouter

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,5 @@
 import type { Request, Response } from "express";
 import * as userModel from "../models/userModel.js";
-import { json } from "zod";
 
 export const getUsers = async (req: Request, res: Response) => {
   try {
@@ -12,12 +11,43 @@ export const getUsers = async (req: Request, res: Response) => {
   }
 };
 
+export const getUser = async (req: Request, res: Response) => {
+  try {
+    const user = await userModel.getUserById(Number(req.params.id));
+    if (!user) return res.status(400).json({ message: "User not found" });
+    console.log("User not found");
+    res.json(user);
+  } catch (error) {
+    res.status(400).json({ message: (error as Error).message });
+  }
+};
+
 export const createUser = async (req: Request, res: Response) => {
   try {
     const user = await userModel.createUser(req.body);
     res.status(201).json(user);
-    console.log(`User created with name:${user.id}`);
+    console.log(`User created with id:${user.id}`);
   } catch (err) {
     res.status(400).json({ message: (err as Error).message });
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const user = await userModel.updateUser(Number(req.params.id), req.body);
+    res.json(user);
+    console.log(`User updated with id:${user.id}`);
+  } catch (error) {
+    res.status(400).json({ message: (error as Error).message });
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const user = await userModel.deleteUser(Number(req.params.id));
+    res.json({ message: "User deleted" });
+    console.log(`User deleted`);
+  } catch (error) {
+    res.status(400).json({ message: (error as Error).message });
   }
 };
--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -4,4 +4,7 @@ import * as UserController from "../controllers/userController.js";
 export const userRouter = Router();
 
 userRouter.get("/", UserController.getUsers);
+userRouter.get("/:id", UserController.getUser);
 userRouter.post("/", UserController.createUser);
+userRouter.put("/:id", UserController.updateUser);
+userRouter.delete("/:id", UserController.deleteUser);
